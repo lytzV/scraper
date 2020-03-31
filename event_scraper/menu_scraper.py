0b7ffde55d1d750b8c88060039219416b9bdd17e
Victor_Li <lytz@berkeley.edu>
2020-03-31 15:55:51 -0700

    intelligent update for new and disappearing dishes

--- event_scraper/menu_scraper.py
+++ event_scraper/menu_scraper.py
@@ -74,52 +74,46 @@ def get_pages(pages):
             pages_to_scrape.append(l[:-1]) 
     return pages_to_scrape
 
+def scrape_meal(meal, df, loc):
+    #meal must be Breakfast, Brunch, Dinner
+    assert meal == "Breakfast" or meal == "Brunch or Lunch" or meal == "Dinner"
+    if meal == "Brunch or Lunch":
+        prefix = "//div[@id='node-177']/div/div[5]/div[h3[@class='location_period'] = ' Lunch' or h3[@class='location_period'] = ' Brunch']/"
+    else:
+        prefix = "//div[@id='node-177']/div/div[5]/div[h3[@class='location_period'] = ' {}']/".format(meal)
+    xpaths = {
+        "loc": prefix+"h3[@class='location2']",
+        "menu": prefix+"p",
+        "type": prefix+"p[@class='station_wrap']",
+        "msg": prefix+"p[not(@class='station_wrap')]"
+    }
+
+    menu = [{t.text:[link.get_attribute("title") for link in t.find_elements_by_class_name('food_icon')]} for t in driver.find_elements_by_xpath(xpaths["menu"])]
+    m = [t.text for t in driver.find_elements_by_xpath(xpaths["msg"])]
+    msg = "No {} Menu".format(meal) if len(menu) == 0 else (m[0] if len(m)!=0 else "No Message")
+    station = [t.text for t in driver.find_elements_by_xpath(xpaths["type"])]
+    
+    df = parse(loc, menu, station, msg, df, meal)
+    return df
+
+
 def scrape(pages, driver):
-    prefix_brunch = "//div[@id='node-177']/div/div[5]/div[h3[@class='location_period'] = ' Lunch' or h3[@class='location_period'] = ' Brunch']/"
-    xpaths_brunch = {
-        "loc": prefix_brunch+"h3[@class='location2']",
-        "menu": prefix_brunch+"p",
-        "msg": prefix_brunch+"p[not(@class='station_wrap')]"
-
-    }
-    prefix_dinner = "//div[@id='node-177']/div/div[5]/div[h3[@class='location_period'] = ' Dinner']/"
-    xpaths_dinner = {
-        "loc": prefix_dinner+"h3[@class='location2']",
-        "menu": prefix_dinner+"p",
-        "msg": prefix_dinner+"p[not(@class='station_wrap')]"
-
-    }
-    prefix_breakfast = "//div[@id='node-177']/div/div[5]/div[h3[@class='location_period'] = ' Breakfast']/"
-    xpaths_breakfast = {
-        "loc": prefix_breakfast+"h3[@class='location2']",
-        "menu": prefix_breakfast+"p",
-        "msg": prefix_breakfast+"p[not(@class='station_wrap')]"
-
-    }
-    breakfast_df = pd.DataFrame(None, columns=['loc','is_open','breakfast','hot_grains','muffin','danish','entrees','byo','msg'])
-    brunch_df = pd.DataFrame(None, columns=['loc','is_open','breakfast','entrees','pizza','muffin','hot_grains','soups','deli_salad','danish','bear_fit','msg'])
-    dinner_df = pd.DataFrame(None, columns=['loc','is_open','soups','entrees','pizza','deli_salad','bear_fit','grilled','pastas','dessert','msg'])
+    breakfast_df = pd.DataFrame(None, columns=['LOC','IS_OPEN','MSG'])
+    brunch_df = pd.DataFrame(None, columns=['LOC','IS_OPEN','MSG'])
+    dinner_df = pd.DataFrame(None, columns=['LOC','IS_OPEN','MSG'])
 
     try:
         for p in pages:
             driver.get(p)
             WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'menu_wrap_overall')))
+
             loc = link_2_dh[p]
-
-            breakfast_menu = [{t.text:[link.get_attribute("title") for link in t.find_elements_by_class_name('food_icon')]} for t in driver.find_elements_by_xpath(xpaths_breakfast["menu"])]
-            m = [t.text for t in driver.find_elements_by_xpath(xpaths_breakfast["msg"])]
-            breakfast_msg = "No breakfast menu" if len(breakfast_menu) == 0 else (m[0] if len(m)!=0 else "No message")
-            breakfast_df = parse(loc, breakfast_menu, breakfast_msg, breakfast_df, "breakfast")
-
-            lunch_menu = [{t.text:[link.get_attribute("title") for link in t.find_elements_by_class_name('food_icon')]} for t in driver.find_elements_by_xpath(xpaths_brunch["menu"])]
-            m = [t.text for t in driver.find_elements_by_xpath(xpaths_brunch["msg"])]
-            lunch_msg = "No lunch menu" if len(lunch_menu) == 0 else (m[0] if len(m)!=0 else "No message")
-            brunch_df = parse(loc, lunch_menu, lunch_msg, brunch_df, "brunch")
-            
-            dinner_menu = [{t.text:[link.get_attribute("title") for link in t.find_elements_by_class_name('food_icon')]} for t in driver.find_elements_by_xpath(xpaths_dinner["menu"])]
-            m = [t.text for t in driver.find_elements_by_xpath(xpaths_dinner["msg"])]
-            dinner_msg = "No dinner menu" if len(dinner_menu) == 0 else (m[0] if len(m)!=0 else "No message")
-            dinner_df = parse(loc, dinner_menu, dinner_msg, dinner_df, "dinner")
+            breakfast_df = scrape_meal("Breakfast", breakfast_df, loc)
+            print(loc, "breakfast done")
+            brunch_df = scrape_meal("Brunch or Lunch", brunch_df, loc)
+            print(loc, "lunch done")
+            dinner_df = scrape_meal("Dinner", dinner_df, loc)
+            print(loc, "dinner done")
             
         return breakfast_df, brunch_df, dinner_df
     except Exception as e:
@@ -127,60 +121,31 @@ def scrape(pages, driver):
     driver.quit()
 
 
-def parse(loc, menu_with_options, msg, df, meal):
-    web_to_df = {}
+def parse(loc, menu_with_options, station, msg, df, meal):
+    row = {}
+    row['LOC'] = loc
+    row['MSG'] = msg
     menu = mylist([list(d.keys())[0] for d in menu_with_options])
     
-    if meal == "brunch":
-        row = {'loc':loc,'is_open':True,'breakfast':None,'hot_grains':None,'muffin':None,'entrees':None,'pizza':None,'soups':None,'deli_salad':None,'danish':None,'bear_fit':None,'msg':msg}
-        if len(re.findall(r'No lunch menu',msg)) != 0:
-            row['is_open'] = False
-            df = df.append(row, ignore_index=True)
-            return df
-        row['is_open'] = True
-        labels = [menu.find('HOT GRAINS'), menu.find('BREAKFAST'), menu.find('MUFFIN'), menu.find('ENTREES'), menu.find('PIZZAS'), menu.find('SOUPS'), menu.find('DELI AND SALAD BAR'), menu.find('DANISH'), menu.find('BEAR FIT')]
-        web_to_df['HOT GRAINS'] = 'hot_grains'
-        web_to_df['BREAKFAST'] = 'breakfast'
-        web_to_df['MUFFIN'] = 'muffin'
-        web_to_df['ENTREES'] = 'entrees'
-        web_to_df['PIZZAS'] = 'pizza'
-        web_to_df['SOUPS'] = 'soups'
-        web_to_df['DELI AND SALAD BAR'] = 'deli_salad'
-        web_to_df['DANISH'] = 'danish'
-        web_to_df['BEAR FIT'] = 'bear_fit'
-    elif meal == "breakfast":
-        row = {'loc':loc,'is_open':True,'breakfast':None,'hot_grains':None,'muffin':None,'danish':None,'entrees':None,'byo':None,'msg':msg}
-        if len(re.findall(r'No breakfast menu',msg)) != 0:
-            row['is_open'] = False
-            df.append(row)
-            return 
-        row['is_open'] = True
-        labels = [menu.find('HOT GRAINS'), menu.find('BREAKFAST'), menu.find('MUFFIN'), menu.find('DANISH'), menu.find('ENTREES'), menu.find('BYO BAR')]
-        web_to_df['HOT GRAINS'] = 'hot_grains'
-        web_to_df['BREAKFAST'] = 'breakfast'
-        web_to_df['MUFFIN'] = 'muffin'
-        web_to_df['DANISH'] = 'danish'
-        web_to_df['ENTREES'] = 'entrees'
-        web_to_df['BYO BAR'] = 'byo'
-    else:
-        row = {'loc':loc,'is_open':True,'dessert':None,'pastas':None,'grilled':None,'entrees':None,'pizza':None,'soups':None,'deli_salad':None,'bear_fit':None,'msg':msg}
-        if len(re.findall(r'No dinner menu',msg)) != 0:
-            row['is_open'] = False
-            df.append(row, ignore_index=True)
-            return 
-        row['is_open'] = True
-        labels = [menu.find('DESSERT'), menu.find('PASTAS'), menu.find('GRILLED'), menu.find('BEAR FIT'), menu.find('ENTREES'), menu.find('PIZZAS'), menu.find('SOUPS'), menu.find('DELI AND SALAD BAR')]
-        web_to_df['DESSERT'] = 'dessert'
-        web_to_df['PASTAS'] = 'pastas'
-        web_to_df['GRILLED'] = 'grilled'
-        web_to_df['BEAR FIT'] = 'bear_fit'
-        web_to_df['ENTREES'] = 'entrees'
-        web_to_df['PIZZAS'] = 'pizza'
-        web_to_df['SOUPS'] = 'soups'
-        web_to_df['DELI AND SALAD BAR'] = 'deli_salad'
+    if msg == "No {} Menu".format(meal):
+        row['IS_OPEN'] = False
+        df = df.append(row, ignore_index=True)
+        print("{} has nothing during {}".format(loc, meal))
+        return df
+    row['IS_OPEN'] = True
 
+    if station[0] == "MEAL COVERS":
+        station = station[1:]
+    
+    ### Update DataFrame if sees new stations
+    new_station = [s for s in station if s not in list(df.columns)]
+    df = pd.concat([df, pd.DataFrame(None, columns=new_station)], axis=1)
+    print("{} has {} during {}".format(loc, station, meal))
+
+    labels = [menu.find(s) for s in station]
     labels = [l for l in labels if l != -1]
     if len(labels) == 0:
+        row['IS_OPEN'] = False
         df = df.append(row, ignore_index=True)
         return df
     labels.sort()
@@ -191,12 +156,12 @@ def parse(loc, menu_with_options, msg, df, meal):
         keys = [list(f.keys())[0] for f in food[1:]]
         values = [list(f.values())[0] for f in food[1:]]
         name_to_option = {k:v for (k,v) in zip(keys, values)}
-        row[web_to_df[list(food[0].keys())[0]]] = json.dumps(name_to_option)
+        row[list(food[0].keys())[0]] = json.dumps(name_to_option)
     food = menu_with_options[labels[-1]:]
     keys = [list(f.keys())[0] for f in food[1:]]
     values = [list(f.values())[0] for f in food[1:]]
     name_to_option = {k:v for (k,v) in zip(keys, values)}
-    row[web_to_df[list(food[0].keys())[0]]] = json.dumps(name_to_option)
+    row[list(food[0].keys())[0]] = json.dumps(name_to_option)
     df = df.append(row, ignore_index=True)
 
     return df
@@ -214,7 +179,7 @@ def import_sql(dfs):
             conn.execute('ALTER TABLE Breakfast ADD PRIMARY KEY (loc(5));')
     except Exception as e:
         print(e)
-   
+
 
 if __name__ == "__main__":
     print(datetime.now())
